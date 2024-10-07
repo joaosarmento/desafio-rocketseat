f4f2f1b95bde93f0f7e35882b81626cbf6d803b8
João Sarmento <joaovicentesarm@gmail.com>
2024-10-07 13:30:27 -0300

    First commit

---
+++ src/main/java/com/java02/desafio_rocketseat_java_02/DesafioRocketseatJava02Application.java
@@ -0,0 +1,13 @@
+package com.java02.desafio_rocketseat_java_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRocketseatJava02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRocketseatJava02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/java02/desafio_rocketseat_java_02/DesafioRocketseatJava02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java02.desafio_rocketseat_java_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRocketseatJava02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
