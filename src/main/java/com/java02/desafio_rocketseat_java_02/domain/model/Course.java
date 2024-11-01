000b348ebe5f9f1f44765b4252cc5beb004180c9
GabiFerraz <ferrazmgabi@gmail.com>
2024-11-01 18:32:02 -0300

    fix imports

--- src/main/java/com/java02/desafio_rocketseat_java_02/domain/model/Course.java
+++ src/main/java/com/java02/desafio_rocketseat_java_02/domain/model/Course.java
@@ -1,6 +1,11 @@
 package com.java02.desafio_rocketseat_java_02.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.Objects;
